3dd2ff8e092c43fabe50281f28b3c5340c5c3783
Arminek <arkadiusz.k.e@gmail.com>
2015-12-03 10:10:20 +0100

    Split api test cases into two context

---
+++ src/ApiXmlTestCase.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the ApiTestCase package.
+ *
+ * (c) Lakion
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lakion\ApiTestCase;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Arkadiusz Krakowiak <arkadiusz.krakowiak@lakion.com>
+ */
+abstract class ApiXmlTestCase extends ApiTestCase
+{
+    public function setUp()
+    {
+        $this->client = static::createClient(array(), array('HTTP_ACCEPT' => MediaTypes::XML));
+    }
+
+    /**
+     * @param Response $response
+     * @param string $filename
+     * @param int $statusCode
+     *
+     * @throws \Exception
+     */
+    protected function assertResponse(Response $response, $filename, $statusCode = 200)
+    {
+        if (isset($_SERVER['OPEN_ERROR_IN_BROWSER']) && true === $_SERVER['OPEN_ERROR_IN_BROWSER']) {
+            $this->showErrorInBrowserIfOccurred($response);
+        }
+
+        $this->assertResponseCode($response, $statusCode);
+        $this->assertXmlHeader($response);
+        $this->assertXmlResponseContent($response, $filename);
+
+    }
+
+    /**
+     * @param Response $response
+     */
+    private function assertXmlHeader(Response $response)
+    {
+        parent::assertHeader($response, MediaTypes::XML);
+    }
+
+    /**
+     * @param Response $response
+     * @param $filename
+     *
+     * @throws \Exception
+     */
+    private function assertXmlResponseContent(Response $response, $filename)
+    {
+        parent::assertResponseContent($response, $filename, 'xml');
+    }
+}
