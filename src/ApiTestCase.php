d92da3450ad637ca2375933b20168b4460e2e13e
Ben Davies <ben.davies@gmail.com>
2016-12-11 21:15:13 +0000

    fix entityManager and fixtureLoader memory leaks

--- src/ApiTestCase.php
+++ src/ApiTestCase.php
@@ -120,6 +120,8 @@ abstract class ApiTestCase extends WebTestCase
 
         \Mockery::close();
         $this->client = null;
+        $this->entityManager = null;
+        $this->fixtureLoader = null;
 
         parent::tearDown();
     }
